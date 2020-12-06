462f93a83e8c42d8db43f751bb9d07922e084ab1
hosniadel666 <hosniadel666>
2020-12-07 01:26:42 +0200

    Add server

---
+++ server/models/post.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const { ObjectId } = mongoose.Schema;
+
+
+const postSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    },
+    photo: {
+        data: Buffer,
+        contenType: String
+    },
+    postedBy: {
+        type: ObjectId,
+        ref: 'User'
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    },
+
+    likes: [{ type: ObjectId, ref: 'User' }],
+    comments: [
+        {
+            text: String,
+            postedBy: { type: ObjectId, ref: 'User' }
+        }
+    ]
+});
+
+module.exports = mongoose.model('Post', postSchema);
