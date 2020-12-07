6b73a8785ec6262f677317eb95a9500ae00e6aac
Khaled Emara <mail@KhaledEmara.dev>
2020-12-07 17:42:10 +0200

    Setup ESLint, Prettier, Express, Mongoose, Apollo

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const next = require("next");
+
+const { ApolloServer } = require("apollo-server-express");
+const { typeDefs, resolvers } = require("./backend/schema");
+
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  const apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+
+  apolloServer.applyMiddleware({ server });
+
+  server.all("*", (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
