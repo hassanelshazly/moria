d26bf1ced3c6ce554c2806849c3b1fbda5291ebe
Hassan El-shazly <hassanelshazly2010@gmail.com>
2020-12-11 01:21:36 +0200

    Initialize apollo server

---
+++ server/index.js
@@ -0,0 +1,38 @@
+const { ApolloServer } = require('apollo-server');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+const typeDefs = require('./schema/types/index');
+const resolvers = require('./schema/resolvers/index');
+
+// Configure the enviroment
+dotenv.config();
+const port = process.env.PORT || 4000;
+
+// Create apollo server
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: ({ req }) => ({ req })
+});
+
+
+// Connect to the database
+mongoose.connect(process.env.MONGO_URI_TEMP, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+})
+    .then(() => console.log('DB Connected'))
+    .catch(err => console.error(err));
+
+mongoose.connection.on('error', err => {
+    console.log(`DB connection error: ${err.message}`);
+});
+
+
+// Start apollo server
+server.listen(port, () => {
+    console.log(`Server started on http://localhost:${port}`);
+});
+
