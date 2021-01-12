679e1fac2a315f48ec0775fbdb3e4146d3b841f9
Khaled Emara <mail@KhaledEmara.dev>
2021-01-12 19:13:09 +0200

    Fix Discovery

--- frontend/src/components/Discovery.jsx
+++ frontend/src/components/Discovery.jsx
@@ -1,12 +1,10 @@
-/* eslint-disable no-unused-vars */
-import { Grid, makeStyles, Typography } from "@material-ui/core";
 import React from "react";
 import PropTypes from "prop-types";
+import { Grid, makeStyles, Typography } from "@material-ui/core";
 import UserCard from "./UserCard";
 
 import { gql, useQuery } from "@apollo/client";
 import { connect } from "react-redux";
-import { Redirect } from "react-router-dom";
 
 const useStyles = makeStyles(() => {
   return {
@@ -18,44 +16,24 @@ const useStyles = makeStyles(() => {
     },
   };
 });
+
 const GET_ALL_USERS = gql`
   query GetAllUsers {
-    findAllUsers  { 
+    findAllUsers {
       id
       fullname
-      username
     }
   }
 `;
-const GET_FOLLOWERS = gql`
-  query GetFollowers($bla: String!)
-  {
-    findUser(username: $bla)
-    {
-      following
-      {
-        username
-        fullname
-        id
-      }
-    }
-  }
-`;
+
 function Discovery(props) {
-  // eslint-disable-next-line no-unused-vars
-  const { loading, error, data } = useQuery(GET_ALL_USERS);
   const { user } = props;
-
-  const USERNAME =  "7assan";
-
-  const {loading:followersLoading , error:followersError, data:followersData} =useQuery(GET_FOLLOWERS , {
-    variables: { bla: USERNAME}
-  });
+  const classes = useStyles();
+  const { loading, error, data } = useQuery(GET_ALL_USERS);
 
   if (error) return `Error! ${error.message}`;
-  if(followersError)   return `Error! ${followersError.message}`;
-  if(followersLoading || loading)   return <p>Loading ...</p>;
-  const classes = useStyles();
+  if (loading) return <p>Loading...</p>;
+
   return (
     <Grid container direction={"column"}>
       <Typography className={classes.headerStyle} variant={"h4"}>
@@ -63,11 +41,11 @@ function Discovery(props) {
       </Typography>
 
       <Grid item container spacing={4}>
-        {followersData.findAllUsers.map((someuser) => {
-          if (someuser.id != user.id ) {
+        {data.findAllUsers.map((someuser) => {
+          if (someuser.id != user.id) {
             return (
               <Grid item xs={12} sm={6} md={4}>
-                <UserCard canFollow={!data.findUser.following.includes(someuser)} name={someuser.fullname} username={someuser.username} id={someuser.id} />
+                <UserCard name={someuser.fullname} />
               </Grid>
             );
           }
