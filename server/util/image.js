71857fa4fe936635b800f9c1d7a180b04e0e874f
Hassan El-shazly <hassanelshazly2010@gmail.com>
2021-01-18 06:26:00 +0200

    Add deleteImage function

--- server/util/image.js
+++ server/util/image.js
@@ -1,6 +1,7 @@
 const cloudinary = require("cloudinary");
 
 const cloudinaryBaseUrl = `http://res.cloudinary.com/${process.env.CLOUDINARY_NAME}/`
+const folderName = "imgs"
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_NAME,
@@ -12,9 +13,17 @@ async function uploadImage(imageSrc) {
     let result = await cloudinary.v2.uploader.upload(imageSrc, {
         allowed_formats: ["jpg", "png"],
         public_id: "",
-        folder: "imgs",
+        folder: folderName,
     });
     return cloudinaryBaseUrl + result.public_id;
+}
+
+async function deleteImage(imageUrl) {
+    let idx = imageUrl.lastIndexOf("/");
+    let public_id = folderName + imageUrl.slice(idx);
+    console.log(public_id)
+    let result = await cloudinary.v2.uploader.destroy(public_id);
+    return result;
 }
 
 module.exports = {
